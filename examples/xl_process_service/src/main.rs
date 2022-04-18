a26302ef17f31f7a3df6284dc0af4e59f98fdd35
Jacob Xie <jacobbishopxy@gmail.com>
2022-04-18 17:02:52 +0800

    chore

--- examples/xl_process_service/src/main.rs
+++ examples/xl_process_service/src/main.rs
@@ -4,23 +4,34 @@
 //!
 //! Reference: https://github.com/actix/examples/blob/master/forms/multipart/src/main.rs
 
-use std::fs::File;
 use std::io::Write;
+use std::{fmt::Display, fs::File};
 
 use actix_multipart::Multipart;
 use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 use futures_util::TryStreamExt;
 use uuid::Uuid;
 
-// use fabrix::xl::XlSource;
+use fabrix::FabrixError;
+
+#[derive(Debug)]
+struct WebError {
+    err: FabrixError,
+}
+
+impl Display for WebError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.err)
+    }
+}
+
+impl actix_web::error::ResponseError for WebError {}
 
 async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
     // iterate over multipart stream
     while let Some(mut field) = payload.try_next().await? {
         // A multipart/form-data stream has to contain `content_disposition`
-        let content_disposition = field
-            .content_disposition()
-            .ok_or_else(|| HttpResponse::BadRequest().finish())?;
+        let content_disposition = field.content_disposition();
 
         let filename = content_disposition
             .get_filename()
@@ -31,13 +42,29 @@ async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
         dbg!(&filepath);
 
         // File::create is blocking operation, use threadpool
-        let mut f = web::block(|| File::create(filepath)).await?;
+        let mut f = web::block(|| File::create(filepath)).await??;
+
+        // let source = XlSource::File(f);
+
+        // let mut xle = XlJsonExecutor::new_with_source(source).map_err(|e| WebError { err: e })?;
+
+        // let res = xle.consume_fn(
+        //     Some(30),
+        //     "data",
+        //     |d| Ok(XlJsonConvertor::transform_data(d)),
+        //     |d| {
+        //         println!("{:?}\n\n", d);
+        //         Ok(())
+        //     },
+        // );
+
+        // println!("{:?}", res);
 
         // Field in turn is stream of *Bytes* object
         while let Some(chunk) = field.try_next().await? {
             dbg!(&chunk);
             // filesystem operations are blocking, we have to use threadpool
-            f = web::block(move || f.write_all(&chunk).map(|_| f)).await?;
+            f = web::block(move || f.write_all(&chunk).map(|_| f)).await??;
         }
 
         // read from cached file
@@ -46,7 +73,7 @@ async fn save_file(mut payload: Multipart) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().into())
 }
 
-fn index() -> HttpResponse {
+async fn index() -> HttpResponse {
     let html = r#"
         <html>
         <head><title>Multipart Form</title></head>
