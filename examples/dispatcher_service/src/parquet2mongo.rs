04cdf905da0e15b50ab44ee51e208ca66f86631e
Jacob Xie <jacobbishopxy@gmail.com>
2022-06-29 09:10:52 +0800

    bug: check parquet file

--- examples/dispatcher_service/src/parquet2mongo.rs
+++ examples/dispatcher_service/src/parquet2mongo.rs
@@ -20,42 +20,32 @@ pub async fn parquet_to_mongo(mut payload: Multipart) -> Result<HttpResponse> {
     while let Ok(Some(mut field)) = payload.try_next().await {
         let cd = field.content_disposition();
 
-        let fe = cd.get_filename().map(|n| {
-            let mut iter = n.split('.');
-            let name = iter.next();
-            let extension = iter.next();
-            (name, extension)
-        });
+        let fe = cd.get_filename().map(|n| n.split('.').collect::<Vec<_>>());
 
         if let Some(ss) = fe {
-            match ss {
-                (Some(n), Some(e)) => {
-                    if e != "parquet" {
-                        continue;
-                    }
+            if !matches!(ss.last(), Some(&"parquet")) {
+                continue;
+            }
 
-                    if let Some(MULTIPART_KEY_FILE) = cd.get_name() {
-                        let name = sanitize_filename::sanitize(n);
+            if let Some(MULTIPART_KEY_FILE) = cd.get_name() {
+                let name = sanitize_filename::sanitize(cd.get_filename().unwrap());
 
-                        let mut buff = Cursor::new(Vec::new());
+                let mut buff = Cursor::new(Vec::new());
 
-                        while let Some(Ok(chunk)) = field.next().await {
-                            buff.get_mut().write_all(&chunk)?;
-                        }
-
-                        let mut reader = ParquetReader::new(buff);
-                        let fx = reader.finish(None).map_err(AppError::Fabrix)?;
-
-                        let mut writer = MongoWriter::new(MG_CONN, MG_DATABASE, MG_COLLECTION)
-                            .await
-                            .map_err(AppError::Fabrix)?;
-                        writer.finish(fx).await.map_err(AppError::Fabrix)?;
-
-                        let res = UploadedFile::new(name, get_current_time());
-                        result.push(res);
-                    }
+                while let Some(Ok(chunk)) = field.next().await {
+                    buff.get_mut().write_all(&chunk)?;
                 }
-                _ => continue,
+
+                let mut reader = ParquetReader::new(buff);
+                let fx = reader.finish(None).map_err(AppError::Fabrix)?;
+
+                let mut writer = MongoWriter::new(MG_CONN, MG_DATABASE, MG_COLLECTION)
+                    .await
+                    .map_err(AppError::Fabrix)?;
+                writer.finish(fx).await.map_err(AppError::Fabrix)?;
+
+                let res = UploadedFile::new(name, get_current_time());
+                result.push(res);
             }
         }
     }
