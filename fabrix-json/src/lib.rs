ce315f926962c3dbc68aca2bed7b56c487d9ecd7
Jacob Xie <jacobbishopxy@gmail.com>
2022-07-08 11:32:46 +0800

    chore: udpate deps

--- fabrix-json/src/lib.rs
+++ fabrix-json/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+//! Json
+//!
+//! Fabrix -> Json, Json -> Fabrix
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub mod error;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use error::*;
